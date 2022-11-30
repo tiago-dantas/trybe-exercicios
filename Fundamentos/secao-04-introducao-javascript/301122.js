a25c59dc7c19ec778f6e39698b2eb7c8a9f02b40
Tiago Dantas <tiago.dantas.estacado@gmail.com>
2022-11-30 15:56:03 -0300

    atualizando 291122

--- Fundamentos/secao-04-introducao-javascript/301122.js
+++ Fundamentos/secao-04-introducao-javascript/301122.js
@@ -42,15 +42,48 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema, vamos iniciar a variável com o primeiro valor do array. Dessa forma, podemos pular a posição 0 e iniciar com index = 1
 
-let higherNumber = numbers[0];
+// let higherNumber = numbers[0];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  if (numbers[index] > higherNumber) {
-    higherNumber = numbers[index];
-  }
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+// const n = 9 ;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+
+// const n = 5;
+
+// let resultado = 0;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado += index;
+// };
+// console.log(resultado)
+
+// let a = 5;
+// let b = 7;
+// let c = 3;
+// let d = 9;
+
+// if (d > 8 && c == 3 || a >= 10) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+const final = 100;
+let resultado = 0;
+
+for (let index = 0; index <= final; index += 1) {
+  resultado += index
 }
 
-console.log(higherNumber);
-
-
-
+console.log(resultado);
